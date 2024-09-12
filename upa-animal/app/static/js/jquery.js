a63d3a6f1dce2750db2d6ef317f988682012f336
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2024-09-12 18:00:13 -0300

    a

---
+++ upa-animal/app/static/js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Aplicar máscara de telefone
+  $('#inputTEL').mask('(00)00000-0000');
+
+  // Aplicar máscara de CPF
+  $('#inputCPF').mask('000.000.000-00', {reverse: true});
+});

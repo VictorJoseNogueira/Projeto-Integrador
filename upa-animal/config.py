099d07e711bbdf304d26d3be6c29bf012d584c10
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2024-10-14 04:07:48 -0300

    teste

--- upa-animal/config.py
+++ upa-animal/config.py
@@ -1,4 +1,4 @@
-import os
+"""import os
 
 DEBUG = os.environ.get("DEBUG")
 # Configurações do MySQL
@@ -16,3 +16,20 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 SESSION_TYPE = os.environ.get("SESSION_TYPE")
+"""
+DEBUG = True
+# Configurações do MySQL
+MYSQL_DATABASE_PORT = "25721"
+MYSQL_DATABASE = "railway"
+MYSQL_HOST = "junction.proxy.rlwy.net"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "rKhHzqnEBZCyyZoBnBLGAeXbXscJsqAw"
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rKhHzqnEBZCyyZoBnBLGAeXbXscJsqAw@junction.proxy.rlwy.net:25721/railway'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = 'zjoQB"*%I>}cjpRwm(M#V/U^:f+mc%mKRkoh/XDgKF{gVupo-f}+R?f&u,ng*dd'
+
+
+SESSION_TYPE = 'filesystem'

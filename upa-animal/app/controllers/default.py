18d7d622eb84cfc9dc8df4efe78e8b8eda971cec
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2024-11-15 02:24:20 -0300

    1

--- upa-animal/app/controllers/default.py
+++ upa-animal/app/controllers/default.py
@@ -6,6 +6,7 @@ from flask_login import login_user, login_required, logout_user  # type: ignore
 from werkzeug.exceptions import Unauthorized
 from datetime import datetime
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
